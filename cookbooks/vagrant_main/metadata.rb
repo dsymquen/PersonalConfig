1b868f653555b57ffc1c9f20205c387eae48ab84
Sagar Singh <dsymquen@gmail.com>
2014-02-17 02:01:28 -0500

    adding cookbooks for vagrant

---
+++ cookbooks/vagrant_main/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Sagar Singh" 
+maintainer_email "dsymquen@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures vagrant_main"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
